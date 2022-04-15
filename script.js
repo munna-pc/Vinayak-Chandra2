d534f58e2e56e1c4fca633079252b5d57d8badd4
munna-pc <103735573+munna-pc@users.noreply.github.com>
2022-04-15 15:24:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+let myage = 10;
+if(myage <= 12 && myage >= 1){
+     console.log("You are a kid");
+}
+else if(myage <= 19 && myage > 12){
+     console.log("You are a teen ager");
+}
+else if (myage < 40 && myage > 19){
+     console.log("You are a adult");
+}
+else if(myage < 100 && myage > 40){
+     console.log("You are a old man");
+}
+else{
+     console.log("Wrong Age");
+}
